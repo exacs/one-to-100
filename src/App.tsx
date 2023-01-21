64ce20e98d2a6980ab1c2a957a82684064d70729
Carlos Saito <carlos@saito.style>
2023-01-21 12:19:04 +0100

    Game logic ish

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
+function Game() {
+  const numbers = Array.from({ length: 100 }).map((_, i) => i + 1);
+  const [latest, setLatest] = React.useState(0);
+
+  function handleClick(n: number) {
+    if (n === latest + 1) {
+      // Correct
+      setLatest(n);
+    } else {
+      // No correct
+      setLatest(100);
+    }
+  }
+
+  return (
+    <ul className="grid">
+      {numbers.map((n) => (
+        <li>
+          <button onClick={() => handleClick(n)} disabled={n <= latest}>
+            {n}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export function App() {
-  return <div>Hello world</div>;
+  const [started, setStarted] = React.useState(false);
+
+  return (
+    <>
+      <aside>Timer: 4:00</aside>
+      <main>
+        {!started && (
+          <button onClick={() => setStarted(true)}>Start game</button>
+        )}
+        {started && <Game />}
+      </main>
+    </>
+  );
 }
