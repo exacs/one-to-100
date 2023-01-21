08311472ae850fd1296aaf90ccc3e900bea758ea
Carlos Saito <carlos@saito.style>
2023-01-21 21:01:40 +0100

    Implement timer

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,13 @@ function generateNumbers() {
   return shuffle(Array.from({ length: 100 }).map((_, i) => i + 1));
 }
 
+function formatTime(seconds: number) {
+  const s = (seconds % 60).toString(10).padStart(2, "0");
+  const m = Math.floor(seconds / 60);
+
+  return `${m}:${s}`;
+}
+
 interface GameOutcome {
   /** True if won */
   win: boolean;
@@ -33,22 +40,42 @@ interface GameParams {
   onFinish(GameOutcome);
 }
 
-// Outcome of the game:
-// Win(remaining_time)
-// Lose(score, time)
+function useTime(initialTime: number) {
+  const [time, setTime] = React.useState(initialTime);
+
+  React.useEffect(() => {
+    const t = setInterval(() => {
+      setTime((time) => time - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(t);
+    };
+  }, []);
+
+  return time;
+}
+
 function Game({ onFinish }: GameParams) {
   const [score, setScore] = React.useState(0);
   const [numbers] = React.useState(generateNumbers());
+  const time = useTime(240);
+
+  React.useEffect(() => {
+    if (time <= 0) {
+      onFinish({ win: false, score, time: 0 });
+    }
+  }, [time]);
 
   function handleClick(n: number) {
     if (n === score + 1) {
       setScore(n);
 
       if (n === 100) {
-        onFinish({ win: true, score: 100, time: 0 });
+        onFinish({ win: true, score: 100, time });
       }
     } else {
-      onFinish({ win: false, time: 0, score });
+      onFinish({ win: false, time, score });
     }
   }
 
@@ -56,7 +83,7 @@ function Game({ onFinish }: GameParams) {
     <div className="game">
       <div className="game-score">
         <div className="score">Score: {score}</div>
-        <div className="time">4:00</div>
+        <div className="time">{formatTime(time)}</div>
       </div>
       <ul className="grid">
         {numbers.map((n) => (
@@ -84,6 +111,7 @@ export function App() {
           <Game
             onFinish={(outcome) => {
               console.log(outcome);
+              setStarted(false);
             }}
           />
         )}
